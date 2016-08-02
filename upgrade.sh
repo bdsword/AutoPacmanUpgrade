f8942607f1670d9607f6e676bd685593945df30e
BDSword <thetruebloodsword@gmail.com>
2016-08-02 20:19:32 +0800

    Add wired connection detect

--- upgrade.sh
+++ upgrade.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 
-upgradable_aps=("DSNS Lab AP" "DSNS Lab AP-5G" "Free Wifi")
-
-now_ap=$(iwgetid | sed 's/wlp3s0\s\+ESSID:"\(.\+\)"/\1/g')
-
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-for ((i = 0; i < ${#upgradable_aps[@]}; i++))
-do
-    if [[ ${upgradable_aps[$i]} == $now_ap ]]; then
-        pacman -Syu --noconfirm
-        break
+should_upgrade=0
+
+if [[ $(cat /sys/class/net/enp4s0f2/carrier) == 1 ]]; then
+    wget -q --tries=10 --timeout=20 --spider http://google.com
+    if [[ $? -eq 0 ]]; then
+        should_upgrade=1
     fi
-done
+else
+    upgradable_aps=("DSNS Lab AP" "DSNS Lab AP-5G" "FREE WIFI")
+
+    now_ap=$(iwgetid | sed 's/wlp3s0\s\+ESSID:"\(.\+\)"/\1/g')
+
+    for ((i = 0; i < ${#upgradable_aps[@]}; i++))
+    do
+        if [[ ${upgradable_aps[$i]} == $now_ap ]]; then
+            should_upgrade=1
+            break
+        fi
+    done
+fi
+
+if [[ $should_upgrade == 1 ]]; then
+    pacman -Syu --noconfirm
+fi
